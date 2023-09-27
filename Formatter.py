2ea0eb61e6fee23d05a2b1cc02ef730da1b3196a
Jacob Bailey <jacobrbailey@icloud.com>
2023-09-26 21:05:32 -0500

    Fix mode-select menu bug and print statement string layout
    > Comment out regex replace func from mode select menu dictionary
     - Throws an exception since the function isn't defined due to being commented out
       as it is still in progress and not yet functional
    > Edit print statement to fix visual aesthetic and readability

--- Formatter.py
+++ Formatter.py
@@ -38,7 +38,7 @@ def inputCorrectValidation(prompt, inputType):
         else:
             return userInput
 
-def filewalk(directory):
+def filewalk(directory): #TODO: Determine way to hide hidden filetypes such as .DS_Store
     for root, dirs, files in os.walk(directory):
         directoryFiles = files
     return directoryFiles
@@ -146,9 +146,9 @@ def listFormatter():
 #Print a fun formatter tool message to screen
 print('- - - - - - - - - - -\n- - - FORMATTER - - -\n- - - - - - - - - - -')
 #Have user select mode, execute selection mode function, and save to variable
-modeDictionary = {'List Formatter':listFormatter,'Template Inputter':templateInputter,'Regex Replacement':regexReplace}
+modeDictionary = {'List Formatter':listFormatter,'Template Inputter':templateInputter}#,'Regex Replacement':regexReplace} #TODO: Replace when Regex module is activated
 listSelection = pyip.inputMenu(list(modeDictionary.keys()), prompt='Select formatter mode:\n', numbered=True)
 returnValue = modeDictionary[listSelection]()
 #Print and copy formatter return to clipboard
 pyperclip.copy(returnValue)
-print(f'\n--------------------------\n\n{returnValue}\n\n--------------------------\\n to clipboard.\n')
+print(f'\n--------------------------\n\n{returnValue}\n\n--------------------------\n\n Copied to clipboard.\n')
